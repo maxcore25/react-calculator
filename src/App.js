9bd0b701347c4af418037463cec6c3109fa6cdec
maxcore25 <mkorobov2002@mail.ru>
2022-02-14 06:11:19 +0300

    Calculator Buttons set

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ function App() {
       <button className='span-two'>AC</button>
       <button>DEL</button>
       <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className='span-two'>=</button>
     </div>
   );
 }
