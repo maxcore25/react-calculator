dd4201e83f55d0901251c43c22e4d8cd3695c267
maxcore25 <mkorobov2002@mail.ru>
2022-02-14 14:33:49 +0300

    Format Operand Function Bug fixed

--- src/App.js
+++ src/App.js
@@ -119,6 +119,7 @@ function formatOperand(operand) {
   if (operand == null) return;
   const [integer, decimal] = operand.split('.');
   if (decimal == null) return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
 }
 
 function App() {
